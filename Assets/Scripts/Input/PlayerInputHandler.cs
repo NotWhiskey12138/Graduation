2787deea17816eae96b89ef839574d0465c44045
NotWhiskey12138 <137181834+NotWhiskey12138@users.noreply.github.com>
2024-05-15 00:28:18 +0800

    重置项目
    写了状态机的移动和IDLE状态

---
+++ Assets/Scripts/Input/PlayerInputHandler.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputHandler : MonoBehaviour
+{
+    public Vector2 MovementInput { get; private set; }
+    
+    public void OnMoveInput(InputAction.CallbackContext context)
+    {
+        MovementInput = context.ReadValue<Vector2>();
+        
+    }
+
+    public void OnJumpInput(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            
+        }
+
+        if (context.performed)
+        {
+            
+        }
+
+        if (context.canceled)
+        {
+            
+        }
+        Debug.Log("Jump");
+    }
+}
