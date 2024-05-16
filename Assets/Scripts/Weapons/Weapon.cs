ce79226b99256ff24b49d3bbb25a9dc08f629306
NotWhiskey12138 <137181834+NotWhiskey12138@users.noreply.github.com>
2024-05-16 11:00:41 +0800

    增加攻击移动功能

--- Assets/Scripts/Weapons/Weapon.cs
+++ Assets/Scripts/Weapons/Weapon.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
+    [SerializeField]
+    private SO_WeaponData weaponData;
+        
+    
     protected Animator baseAnimator;
     protected Animator weaponAnimator;
 
     protected PlayerAttackState state;
+
+    protected int attackCounter;
     
     protected void Start()
     {
@@ -21,9 +27,17 @@ public class Weapon : MonoBehaviour
     public virtual void EnterWeapon()
     {
         gameObject.SetActive(true);
+
+        if (attackCounter >= weaponData.movementSpeed.Length)
+        {
+            attackCounter = 0;
+        }
         
         baseAnimator.SetBool("attack",true);
         weaponAnimator.SetBool("attack", true);
+        
+        baseAnimator.SetInteger("attackCounter",attackCounter);
+        weaponAnimator.SetInteger("attackCounter",attackCounter);
     }
 
     public virtual void ExitWeapon()
@@ -31,6 +45,8 @@ public class Weapon : MonoBehaviour
         baseAnimator.SetBool("attack", false);
         weaponAnimator.SetBool("attack", false);
 
+        attackCounter++;
+        
         gameObject.SetActive(false);
     }
 
@@ -41,6 +57,26 @@ public class Weapon : MonoBehaviour
         state.AnimationFinishTrigger();
     }
 
+    public virtual void AnimationStartMovementTrigger()
+    {
+        state.SetPlayerVelocity(weaponData.movementSpeed[attackCounter]);
+    }
+
+    public virtual void AnimationStopMovementTrigger()
+    {
+        state.SetPlayerVelocity(0f);
+    }
+
+    public virtual void AnimationTurnOffFlipTrigger()
+    {
+        state.SetFlipCheck(false);
+    }
+
+    public virtual void AnimationTurnOnFlipTrigger()
+    {
+        state.SetFlipCheck(true);
+    }
+    
     #endregion
 
     public void InitializeWeapon(PlayerAttackState state)
