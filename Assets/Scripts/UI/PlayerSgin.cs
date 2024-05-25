31e3125dca5c1f7c6cf8410ff2921341b0a04b30
NotWhiskey12138 <137181834+NotWhiskey12138@users.noreply.github.com>
2024-05-25 15:09:43 +0800

    标识显示
    新增标识显示但有BUG

---
+++ Assets/Scripts/UI/PlayerSgin.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.DualShock;
+
+public class PlayerSgin : MonoBehaviour
+{
+    private Animator anim;
+
+    public Transform playerTrans;
+    
+    public GameObject signSprite; 
+    
+    private bool canPress;
+
+    private void Awake()
+    {
+        anim = signSprite.GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        signSprite.GetComponent<SpriteRenderer>().enabled = canPress;
+        signSprite.transform.localScale = playerTrans.localScale;
+
+        OnActionChange(obj, InputActionChange.ActionStarted);
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Interactable"))
+        {
+            canPress = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        canPress = false;
+    }
+
+    private void OnActionChange(object obj, InputActionChange actionChange)
+    {
+        if (actionChange == InputActionChange.ActionStarted)
+        {
+            //Debug.Log(((InputAction)obj).activeControl.device);
+
+            var d = ((InputAction)obj).activeControl.device;
+
+            switch (d.device)
+            {
+                case Keyboard:
+                    anim.Play("Keyboard");
+                    break;
+                case DualShockGamepad:
+                    anim.Play("PS");
+                    break;
+            }
+
+        }
+    }
+}
