b7bf229a1118451b5d3f077cf5377319db84b9a8
NotWhiskey12138 <137181834+NotWhiskey12138@users.noreply.github.com>
2024-06-06 14:21:12 +0800

    播放不同的攻击动画
    播放不同攻击动画，添加时间管理器，在一定时间后，重置攻击次数。

--- Assets/_Scripts/Weapons/Weapon.cs
+++ Assets/_Scripts/Weapons/Weapon.cs
@@ -1,27 +1,47 @@
 using System;
 using UnityEngine;
+using Whiskey.Utilities;
 
 namespace Whiskey.Weapons
 {
     public class Weapon : MonoBehaviour
     {
-        public event Action OnExit;
+        [SerializeField] private int numberOfAttacks; //该武器攻击次数
+        [SerializeField] private float attackCounterResetCooldown; //攻击冷却重置时间
+
+        public int CurrentAttackCounter
+        {
+            get => currentAttackCounter;
+            private set => currentAttackCounter = value >= numberOfAttacks ? 0 : value; 
+        }
+        
+        public event Action OnExit; //该武器退出时函数
         
         private Animator anim;
         private GameObject baseGameObject;
 
         private AnimationEventHandler eventHandler;
+
+        private int currentAttackCounter;
+
+        private Timer attackCounterResetTimer; //攻击重置时间计算器
         
         public void Enter()
         {
             print($"{transform.name} enter");
+            
+            attackCounterResetTimer.StopTimer();
 
             anim.SetBool("active", true);
+            anim.SetInteger("counter", currentAttackCounter);
         }
 
         private void Exit()
         {
             anim.SetBool("active", false);
+
+            CurrentAttackCounter++;
+            attackCounterResetTimer.StartTimer();
             
             OnExit?.Invoke();
         }
@@ -32,16 +52,28 @@ namespace Whiskey.Weapons
             anim = baseGameObject.GetComponent<Animator>();
 
             eventHandler = baseGameObject.GetComponent<AnimationEventHandler>();
+
+            attackCounterResetTimer = new Timer(attackCounterResetCooldown);
         }
 
+        private void Update()
+        {
+            attackCounterResetTimer.Tick();
+        }
+
+        private void ResetAttackCounter() => CurrentAttackCounter = 0;
+        
         private void OnEnable()
         {
             eventHandler.OnFinish += Exit;
+            attackCounterResetTimer.OnTimerDone += ResetAttackCounter;
         }
 
         private void OnDisable()
         {
             eventHandler.OnFinish -= Exit;
+            attackCounterResetTimer.OnTimerDone -= ResetAttackCounter;
+
         }
     }
 }
